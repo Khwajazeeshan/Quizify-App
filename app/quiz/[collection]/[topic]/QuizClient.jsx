dadf7205bec9f0c33ecf3e5ccfcc74d965601e13
khwajazeeshan <khawajazeeshan225@gmail.com>
2026-05-30 22:32:34 +0500

    Smart MCQ Test Preparation

--- app/quiz/[collection]/[topic]/QuizClient.jsx
+++ app/quiz/[collection]/[topic]/QuizClient.jsx
@@ -22,7 +22,7 @@ export default function QuizClient({ topic, topicQuestions }) {
   const [answered, setAnswered] = useState(false);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [showScore, setShowScore] = useState(false);
-  const [timeLeft, setTimeLeft] = useState(25);
+  const [timeLeft, setTimeLeft] = useState(30);
   const [userAnswers, setUserAnswers] = useState([]);
 
   const currentQuestion = shuffledQuestions[currentQuestionIndex];
@@ -54,7 +54,7 @@ export default function QuizClient({ topic, topicQuestions }) {
       setCurrentQuestionIndex(nextQuestion);
       setAnswered(false);
       setSelectedAnswer(null);
-      setTimeLeft(25);
+      setTimeLeft(30);
     } else {
       setShowScore(true);
     }
